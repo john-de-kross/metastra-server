31bf28bc6636f1d271fbea894cc8080469b6ef93
john-de-kross <chukwujekwujohncross@gmail.com>
2025-06-10 19:27:11 +0100

    testing otp verification

--- CONTROLLERS/otpController.js
+++ CONTROLLERS/otpController.js
@@ -29,7 +29,7 @@ exports.sendOtp = async (req, res) => {
             from: process.env.GMAIL,
             to: email,
             subject: 'Your OTP Code',
-            text: `Your OTP code to complete your verification is ${otp}\nIt will expire in 10 minutes`
+            text: `Your OTP code to complete your verification is ${otp}.It will expire in 10 minutes`
         }
 
         try {
--- ROUTES/userRoutes.js
+++ ROUTES/userRoutes.js
@@ -3,9 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const { createUser } = require('../CONTROLLERS/registrationController');
-const { sendOtp } = require('../CONTROLLERS/otpController');
+const { sendOtp, otpVerification } = require('../CONTROLLERS/otpController');
 
 router.post('/register', createUser);
 router.post('/otp', sendOtp);
+router.post('/verify', otpVerification);
+
 
 module.exports = router;
