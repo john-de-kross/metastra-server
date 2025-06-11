1080ec0be3456992fb139ef2378c0574ce29065f
john-de-kross <chukwujekwujohncross@gmail.com>
2025-06-11 23:48:21 +0100

    changes

--- CONTROLLERS/loginController.js
+++ CONTROLLERS/loginController.js
@@ -6,7 +6,8 @@ exports.loginUser = async (req, res, next) => {
         const { email, password } = req.body;
         console.log(email, password)
 
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ email }).select('+password');
+        console.log(user.password)
         
         if (!user) {
             return res.status(404).json({
@@ -17,6 +18,7 @@ exports.loginUser = async (req, res, next) => {
 
         
         const isPassword = await bcrypt.compare(password, user.password);
+        console.log(user.password)
 
         if (!isPassword) {
             return res.status(400).json({
