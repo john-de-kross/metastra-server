e90032ec29d69ddf1322dceb7e19596250276100
john-de-kross <chukwujekwujohncross@gmail.com>
2025-06-12 09:03:48 +0100

    updade on login

--- CONTROLLERS/loginController.js
+++ CONTROLLERS/loginController.js
@@ -1,5 +1,9 @@
 const User = require('../MODELS/userModel');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config({path: './config.env'})
 
 exports.loginUser = async (req, res, next) => {
     try {
@@ -35,9 +39,16 @@ exports.loginUser = async (req, res, next) => {
             })
         }
 
+        const token = jwt.sign(
+            { id: user._id, email: user.email },
+            process.env.JWT_SECRET,
+            {expiresIn: '3h'}
+        )
+
         return res.status(200).json({
             success: true,
-            message: 'Login successful'
+            message: 'Login successful',
+            token
         })
 
     } catch (error) {
