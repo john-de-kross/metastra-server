db7f32bccdd85a4ecc606f12e7e0aadc2c5d295f
john-de-kross <chukwujekwujohncross@gmail.com>
2025-06-21 17:05:38 +0100

    changes

--- APP.js
+++ APP.js
@@ -9,20 +9,22 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
-const allowedOrigins = process.env.NODE_ENV === 'production'
-  ? ['https://metastra.vercel.app']
-  : ['http://localhost:5173'];
+const allowedOrigins = [
+  'http://localhost:5173',            // for local dev
+  'https://metastra.vercel.app'      // for production
+];
 
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS')); 
+      callback(new Error('Not allowed by CORS'));
     }
   },
   credentials: true
 }));
+
 
 
 app.use(cookieParser());
