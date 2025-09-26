721c6384ac4ddddf713f9b8bb058662ed0df2204
john-de-kross <chukwujekwujohncross@gmail.com>
2025-09-26 23:10:51 +0100

    changes

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const http = require("http");
 const dotenv = require("dotenv");
 const { Server } = require("socket.io");
 const User = require("./MODELS/userModel");
+const Message = require("./MODELS/messageModel");
 dotenv.config({ path: "./config.env" });
-
 
 mongoose
   .connect(process.env.MONGODB_URL)
@@ -22,51 +22,61 @@ const io = new Server(server, {
   },
 });
 
-
- 
 io.on("connection", (socket) => {
   console.log("User connected", socket.id);
 
   socket.on("register", (userId) => {
     userSocketMap.set(userId, socket.id);
-    console.log("registered Id:", userId)
+    console.log("registered Id:", userId);
 
     io.emit("user-online", userId);
-    console.log("userSocketMap:", [...userSocketMap.keys()])
+    console.log("userSocketMap:", [...userSocketMap.keys()]);
   });
 
-  socket.on('send_message', (data) => {
+  socket.on("send_message", (data) => {
     const user = userSocketMap.get(data.userId);
     if (user) {
-      io.to(user).emit("receive_message", data)
-
+      io.to(user).emit("receive_message", data);
     }
-  })
+  });
 
   socket.on("get-user-status", (userId, callback) => {
     const isOnline = userSocketMap.has(userId);
     const lastSeen = lastSeenMap.get(userId) || null;
     callback({ isOnline, lastSeen });
-  })
+  });
 
   socket.on("typing", (data) => {
     const user = userSocketMap.get(data.receiverId);
     if (user) {
       io.to(user).emit("typing", data);
     }
-  })
+  });
+
+  socket.on("markAsSeen", ({ messageId, chatId, userId }) => {
+    const user = userSocketMap.get(userID);
+    if (user) {
+      io.to(user).emit("messageSeen", { messageId, userId });
+      Message.findByIdAndUpdate(messageId, { seen: true }, { new: true })
+        .exec()
+        .then((msg) => console.log(msg))
+        .catch((err) => console.log(err));
+    }
+  });
   socket.on("disconnect", () => {
     for (const [userId, id] of userSocketMap.entries()) {
       if (id === socket.id) {
         userSocketMap.delete(userId);
         //store timestamp
         const lastSeen = new Date();
-        lastSeenMap.set(userId, lastSeen)
-        User.findByIdAndUpdate(userId, { lastSeen }, { new: true }).exec().then((user) => {
-          console.log(user)
-        }).catch(err => console.log(err))
+        lastSeenMap.set(userId, lastSeen);
+        User.findByIdAndUpdate(userId, { lastSeen }, { new: true })
+          .exec()
+          .then((user) => {
+            console.log(user);
+          })
+          .catch((err) => console.log(err));
         io.emit("user-offline", { userId, lastSeen });
-        
       }
     }
   });
